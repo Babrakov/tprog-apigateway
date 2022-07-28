e21748a8c538dcbc39914ef645684f6098be26e2
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-07-28 11:29:22 +0300

    Init commit.

---
+++ src/main/java/ga/berlo/tprogerapigateway/TprogerApigatewayApplication.java
@@ -0,0 +1,13 @@
+package ga.berlo.tprogerapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TprogerApigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TprogerApigatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ga/berlo/tprogerapigateway/TprogerApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.berlo.tprogerapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TprogerApigatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
